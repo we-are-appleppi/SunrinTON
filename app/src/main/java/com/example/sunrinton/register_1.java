ae32e698d582f38e322f4d0ce47a814f69edf55e
Yoonwonwoo <51125312+Yoonwonwoo@users.noreply.github.com>
2019-07-19 15:52:53 +0900

    commit

---
+++ app/src/main/java/com/example/sunrinton/register_1.java
@@ -0,0 +1,15 @@
+package com.example.sunrinton;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class register_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_1);
+
+    }
+}
---
+++ app/src/main/java/com/example/sunrinton/register_3.java
@@ -0,0 +1,15 @@
+package com.example.sunrinton;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class register_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_3);
+
+    }
+}

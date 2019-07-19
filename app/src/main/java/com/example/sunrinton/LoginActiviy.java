501bfb9ca0dccc637eb4daa986c4e2fea0b63ca0
Yoonwonwoo <51125312+Yoonwonwoo@users.noreply.github.com>
2019-07-19 15:31:14 +0900

    commit

---
+++ app/src/main/java/com/example/sunrinton/LoginActiviy.java
@@ -0,0 +1,15 @@
+package com.example.sunrinton;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActiviy extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}

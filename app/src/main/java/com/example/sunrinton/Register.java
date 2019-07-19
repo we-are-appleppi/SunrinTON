2106a9c9b1d781c0bbba634b080f18c30d9a5395
chlwhdtn03 <chlwhdtn03@naver.com>
2019-07-19 19:34:34 +0900

    회원가입 정비

--- app/src/main/java/com/example/sunrinton/Register.java
+++ app/src/main/java/com/example/sunrinton/Register.java
@@ -14,6 +14,8 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -52,28 +54,53 @@ public class Register extends AppCompatActivity {
                 datas.put("password", str_passwd);
                 //datas.put("key", timestamp);
 
+
+
                 if (!android.util.Patterns.EMAIL_ADDRESS.matcher(str_email).matches()) {
                     Toast.makeText(Register.this, "잘못된 이메일형식 입니다", Toast.LENGTH_SHORT).show();
                 } else {
-                    db.collection("accounts").document(str_email)
-                            .set(datas)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    Intent intent = new Intent();
-                                    intent.putExtra("email", str_email);
-                                    intent.putExtra("password", str_passwd);
-                                    intent.putExtra("name", str_name);
-                                    setResult(RESULT_OK, intent);
-                                    finish();
+                    DocumentReference docRef = db.collection("accounts").document(str_email);
+                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if(task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if(!document.exists()) {
+                                    if(str_name.isEmpty() || str_name == null) {
+                                        Toast.makeText(Register.this, "이름을 입력해 주세요", Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }
+
+
+                                    if(str_passwd.isEmpty() || str_passwd == null) {
+                                        Toast.makeText(Register.this, "비밀번호를 입력해 주세요", Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }
+                                    db.collection("accounts").document(str_email)
+                                            .set(datas)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    Intent intent = new Intent();
+                                                    intent.putExtra("email", str_email);
+                                                    intent.putExtra("password", str_passwd);
+                                                    intent.putExtra("name", str_name);
+                                                    setResult(RESULT_OK, intent);
+                                                    finish();
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(Register.this, "오류가 발생하였습니다.", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                } else {
+                                    Toast.makeText(Register.this, "이미 등록된 계정입니다.", Toast.LENGTH_SHORT).show();
                                 }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(Register.this, "오류가 발생하였습니다.", Toast.LENGTH_SHORT);
-                                }
-                            });
+                            }
+                        }
+                    });
                 }
             }
         });
